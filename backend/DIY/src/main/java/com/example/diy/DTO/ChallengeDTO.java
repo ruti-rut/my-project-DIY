25cd09c0710466bf16ad6096ce3064e2ac714c98
yap <yael300500@gmail.com>
2025-11-03 20:31:13 +0200

    challenge DTO

---
+++ backend/DIY/src/main/java/com/example/diy/DTO/ChallengeDTO.java
@@ -0,0 +1,73 @@
+package com.example.diy.DTO;
+
+import com.example.diy.model.Project;
+import jakarta.persistence.OneToMany;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ChallengeDTO {
+    private Long id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String theme;
+    private String content;
+    private List<Project> projects;
+    private String picturePath;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    public String getPicturePath() {
+        return picturePath;
+    }
+
+    public void setPicturePath(String picturePath) {
+        this.picturePath = picturePath;
+    }
+}
