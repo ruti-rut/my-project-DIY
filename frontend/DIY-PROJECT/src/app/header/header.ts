df785ddfd50edfba0df034a5f0766d9f29297486
ruti-rut <ruti0583210@gmail.com>
2025-11-03 18:55:31 +0200

    add com

--- frontend/DIY-PROJECT/src/app/header/header.ts
+++ frontend/DIY-PROJECT/src/app/header/header.ts
@@ -1,11 +1,21 @@
+// src/app/components/header/header.component.ts
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrls: ['./header.css']
 })
-export class Header {
-
-}
+export class HeaderComponent {}
