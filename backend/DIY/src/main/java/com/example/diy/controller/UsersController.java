1c081c712d171d856c30b6ec4201b1fb46ac52ef
ruti-rut <ruti0583210@gmail.com>
2025-11-05 00:03:51 +0200

    upload challenge

--- backend/DIY/src/main/java/com/example/diy/controller/UsersController.java
+++ backend/DIY/src/main/java/com/example/diy/controller/UsersController.java
@@ -1,4 +1,17 @@
 package com.example.diy.controller;
 
+import com.example.diy.DTO.UsersRegisterDTO;
+import com.example.diy.Mapper.UsersMapper;
+import com.example.diy.service.UsersRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
+
 public class UsersController {
+    UsersRepository usersRepository;
+    UsersMapper usersMapper;
+
 }
--- backend/DIY/src/main/java/com/example/diy/service/UsersRepository.java
+++ backend/DIY/src/main/java/com/example/diy/service/UsersRepository.java
@@ -4,4 +4,8 @@ import com.example.diy.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users,Long> {
+    Users findByUserName(String userName);    // מחזיר Users או null
+    Users findByMail(String mail);            // מחזיר Users או null
+    boolean existsByUserName(String userName);
+    boolean existsByMail(String mail);
 }
