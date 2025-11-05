7d671c2cee7f667edec2221a3be646bb038a792d
ruti-rut <ruti0583210@gmail.com>
2025-11-06 00:19:54 +0200

    upload challenge

---
+++ backend/DIY/src/main/java/com/example/diy/security/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.diy.security; // *** ודאי שהנתיב הזה נכון לפרויקט שלך ***
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .cors(cors->cors.configurationSource(request -> {
+                    CorsConfiguration corsConfiguration=new CorsConfiguration();
+                    // ודאי שכתובת ה-Frontend שלך נכונה
+                    corsConfiguration.setAllowedOrigins(List.of("*"));
+                    corsConfiguration.setAllowedMethods(List.of("*"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    return corsConfiguration;
+                }))
+
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(auth ->
+                        // כל בקשה (anyRequest) תורשה לכולם (permitAll)
+                        auth.anyRequest().permitAll()
+                )
+
+                .httpBasic(basic -> basic.disable());
+        http.headers(headers -> headers.frameOptions(frameOption -> frameOption.sameOrigin()));
+
+        return http.build();
+    }
+}
