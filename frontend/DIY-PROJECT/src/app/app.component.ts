fde204920cc11d7cd2dd5b63a1646caaca95354b
yap <yael300500@gmail.com>
2025-12-03 23:05:28 +0200

    'footer'

--- frontend/DIY-PROJECT/src/app/app.component.ts
+++ frontend/DIY-PROJECT/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,RouterOutlet ],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
